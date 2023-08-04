f9b87b86d3398428c8d702fabf3cfcdcc68dbcf9
Infinixhome <bernandus_bayu_nugraha_saputra@teknokrat.ac.id>
2023-08-04 08:48:50 +0700

    quis_pw2_21312011

--- soal3.php
+++ soal3.php
@@ -22,14 +22,17 @@
         $numbers = [18, 45, 29, 61, 47, 34];
         echo "array numbers: ";
         print_r($numbers);
-        // Lakukan Looping di sini
-
-        // output nya nanti seperti ini --> [3, 0, 4, 1, 2, 4]
-
         echo "<br>";
-        // tampilkan di sini
-        echo "Array sisa baginya adalah:  "; 
+
+        $rest = [];
+        foreach ($numbers as $number) {
+            $rest[] = $number % 5;
+        }
+
+        echo "Array sisa baginya adalah: ";
+        print_r($rest);
         echo "<br>";
+
 
         echo "<h3> Soal No 3 Looping Asociative Array </h3>";
         /* 
@@ -46,16 +49,26 @@
 
         */
 
-                /* variabel code jangan diubah */
         $items = [
             ['001', 'Keyboard Logitek', 60000, 'Keyboard yang mantap untuk kantoran', 'logitek.jpeg'], 
             ['002', 'Keyboard MSI', 300000, 'Keyboard gaming MSI mekanik', 'msi.jpeg'],
             ['003', 'Mouse Genius', 50000, 'Mouse Genius biar lebih pinter', 'genius.jpeg'],
             ['004', 'Mouse Jerry', 30000, 'Mouse yang disukai kucing', 'jerry.jpeg']
         ];
-        
-        // Output: 
 
+        foreach ($items as $item) {
+            $itemAssoc = [
+                'id' => $item[0],
+                'name' => $item[1],
+                'price' => $item[2],
+                'description' => $item[3],
+                'source' => $item[4]
+            ];
+
+            echo "<pre>";
+            print_r($itemAssoc);
+            echo "</pre>";
+        }
     ?>
 
 </body>
